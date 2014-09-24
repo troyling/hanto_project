4c0c6f08e72ae707da199be3a245155661b39bd1
Troy Ling <troyling14@gmail.com>
2014-09-24 17:51:01 -0400

    add hantoTestGame and HantoTestGameFactory interfaces

---
+++ test/hanto/common/HantoTestGame.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package hanto.common;
+
+
+/**
+ * Description
+ * @version Sep 21, 2014
+ */
+public interface HantoTestGame extends HantoGame
+{
+	/**
+	 * A data structure used in setting up the the initial configuration of a game
+	 * for testing.
+	 * @version Sep 21, 2014
+	 */
+	class PieceLocationPair {
+		public final HantoPlayerColor player;
+		public final HantoPieceType pieceType;
+		public final HantoCoordinate location;
+		
+		/**
+		 * Default constructor
+		 * @param player the player color
+		 * @param pieceType the piece type
+		 * @param location the coordinate where the piece is at the beginning of the test
+		 */
+		public PieceLocationPair(HantoPlayerColor player, HantoPieceType pieceType,
+				HantoCoordinate location)
+		{
+			this.player = player;
+			this.pieceType = pieceType;
+			this.location = location;
+		}
+	}
+	
+	/**
+	 * Initialize the pieces on the board. This will put the pieces on the board as
+	 * well as updating the store of pieces that are off the board.
+	 * @param initialPieces an array of initial pieces
+	 */
+	void initializeBoard(PieceLocationPair[] initialPieces);
+	
+	/**
+	 * Set the current turn number (whole turn number) beginning at 1
+	 * @param turnNumber
+	 */
+	void setTurnNumber(int turnNumber);
+	
+	/**
+	 * Set the player on the move. This is the player who will next call makeMove().
+	 * @param player the player who will make the next move
+	 */
+	void setPlayerMoving(HantoPlayerColor player);
+}
---
+++ test/hanto/common/HantoTestGameFactory.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package hanto.common;
+
+
+/**
+ * Description
+ * @version Sep 22, 2014
+ */
+public class HantoTestGameFactory
+{
+	private static final HantoTestGameFactory instance = new HantoTestGameFactory();
+	
+	/**
+	 * Default private descriptor.
+	 */
+	private HantoTestGameFactory()
+	{
+		// Empty, but the private constructor is necessary for the singleton.
+	}
+
+	/**
+	 * @return the instance
+	 */
+	public static HantoTestGameFactory getInstance()
+	{
+		return instance;
+	}
+	
+	/**
+	 * Create the specified Hanto game version with the Blue player moving
+	 * first.
+	 * @param gameId the version desired.
+	 * @return the game instance
+	 */
+	public HantoGame makeTestHantoGame(HantoGameID gameId)
+	{
+		return makeHantoTestGame(gameId, HantoPlayerColor.BLUE);
+	}
+	
+	/**
+	 * Factory method that returns the appropriately configured Hanto game.
+	 * @param gameId the version desired.
+	 * @param movesFirst the player color that moves first
+	 * @return the game instance
+	 */
+	public HantoTestGame makeHantoTestGame(HantoGameID gameId, HantoPlayerColor movesFirst) {
+		HantoGame game = null;
+		switch (gameId) {
+			case GAMMA_HANTO:
+				game = new GammaHantoTestGame(movesFirst);
+				break;
+		}
+		return game;
+	}
+}
