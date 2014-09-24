d7a61f12460a77cd83f0d052f311eac638fbc254
Troy Ling <troyling14@gmail.com>
2014-09-24 18:11:38 -0400

    add layout to the test

---
+++ src/hanto/studentAJRZL/gamma/GammaHantoGame.java
@@ -0,0 +1,31 @@
+package hanto.studentAJRZL.gamma;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGame;
+import hanto.common.HantoPiece;
+import hanto.common.HantoPieceType;
+import hanto.common.MoveResult;
+
+public class GammaHantoGame implements HantoGame {
+
+	@Override
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
+			HantoCoordinate to) throws HantoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HantoPiece getPieceAt(HantoCoordinate where) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPrintableBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
