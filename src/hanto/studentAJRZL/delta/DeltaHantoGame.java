6b11ce4e62bd7d661fa7b338831b407218edc227
Anthony J. Ruffa <ajosephruffa@gmail.com>
2014-10-02 10:57:34 -0400

    added needed comments and removed unneeded ones

--- src/hanto/studentAJRZL/delta/DeltaHantoGame.java
+++ src/hanto/studentAJRZL/delta/DeltaHantoGame.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 package hanto.studentAJRZL.delta;
 
 import hanto.common.HantoException;
@@ -5,6 +15,12 @@ import hanto.common.HantoPieceType;
 import hanto.common.HantoPlayerColor;
 import hanto.studentAJRZL.common.BaseHantoGame;
 
+/**
+ * Class for the delta hanto game.
+ * 
+ * @author anthonyjruffa
+ * 
+ */
 public class DeltaHantoGame extends BaseHantoGame {
 	private final int MAX_BOARD_SIZE = 18;
 
--- test/hanto/studentAJRZL/alpha/AlphaHantoMasterTest.java
+++ test/hanto/studentAJRZL/alpha/AlphaHantoMasterTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 package hanto.studentAJRZL.alpha;
 
 import static hanto.common.HantoPieceType.BUTTERFLY;
@@ -17,28 +27,26 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class AlphaHantoMasterTest
-{
-	
+public class AlphaHantoMasterTest {
+
 	/**
 	 * Internal class for these test cases.
+	 * 
 	 * @version Sep 13, 2014
 	 */
-	class TestHantoCoordinate implements HantoCoordinate
-	{
+	class TestHantoCoordinate implements HantoCoordinate {
 		private final int x, y;
-		
-		public TestHantoCoordinate(int x, int y)
-		{
+
+		public TestHantoCoordinate(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
+
 		/*
 		 * @see hanto.common.HantoCoordinate#getX()
 		 */
 		@Override
-		public int getX()
-		{
+		public int getX() {
 			return x;
 		}
 
@@ -46,109 +54,95 @@ public class AlphaHantoMasterTest
 		 * @see hanto.common.HantoCoordinate#getY()
 		 */
 		@Override
-		public int getY()
-		{
+		public int getY() {
 			return y;
 		}
 
 	}
-	
+
 	private static HantoGameFactory factory;
 	private HantoGame game;
-	
+
 	@BeforeClass
-	public static void initializeClass()
-	{
+	public static void initializeClass() {
 		factory = HantoGameFactory.getInstance();
 	}
-	
+
 	@Before
-	public void setup()
-	{
+	public void setup() {
 		game = factory.makeHantoGame(HantoGameID.ALPHA_HANTO);
 	}
-	
+
 	@Test
-	public void blueMakesValidFirstMove() throws HantoException
-	{
+	public void blueMakesValidFirstMove() throws HantoException {
 		final MoveResult mr = game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		assertEquals(OK, mr);
 	}
-	
+
 	@Test
-	public void afterFirstMoveBlueButterflyIsAt0_0() throws HantoException
-	{
+	public void afterFirstMoveBlueButterflyIsAt0_0() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		final HantoPiece p = game.getPieceAt(new TestHantoCoordinate(0, 0));
 		assertEquals(BUTTERFLY, p.getType());
 		assertEquals(HantoPlayerColor.BLUE, p.getColor());
 	}
-	
-	@Test(expected=HantoException.class)
-	public void bluePlacesNonButterfly() throws HantoException
-	{
+
+	@Test(expected = HantoException.class)
+	public void bluePlacesNonButterfly() throws HantoException {
 		game.makeMove(SPARROW, null, new TestHantoCoordinate(0, 0));
 	}
-	
+
 	@Test
-	public void redPlacesButterflyNextToBlueButterfly() throws HantoException
-	{
+	public void redPlacesButterflyNextToBlueButterfly() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 1));
 		final HantoPiece p = game.getPieceAt(new TestHantoCoordinate(0, 1));
 		assertEquals(BUTTERFLY, p.getType());
 		assertEquals(HantoPlayerColor.RED, p.getColor());
 	}
-	
-	@Test(expected=HantoException.class)
-	public void blueAttemptsToPlaceButterflyAtWrongLocation() throws HantoException
-	{
+
+	@Test(expected = HantoException.class)
+	public void blueAttemptsToPlaceButterflyAtWrongLocation() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(-1, 1));
 	}
-	
+
 	@Test
-	public void redMakesValidSecondMoveAndGameIsDrawn() throws HantoException
-	{
+	public void redMakesValidSecondMoveAndGameIsDrawn() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		final MoveResult mr = game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(-1, 1));
 		assertEquals(MoveResult.DRAW, mr);
 	}
-	
-	@Test(expected=HantoException.class)
-	public void redPlacesButterflyNonAdjacentToBlueButterfly() throws HantoException
-	{
+
+	@Test(expected = HantoException.class)
+	public void redPlacesButterflyNonAdjacentToBlueButterfly() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(2, 2));
 	}
-	
-	@Test(expected=HantoException.class)
-	public void attemptToMoveRatherThanPlace() throws HantoException
-	{
+
+	@Test(expected = HantoException.class)
+	public void attemptToMoveRatherThanPlace() throws HantoException {
 		game.makeMove(BUTTERFLY, new TestHantoCoordinate(0, 1), new TestHantoCoordinate(0, 0));
 	}
-	
+
 	/* Tests for coverage */
 	@Test
-	public void redValidMove2() throws HantoException
-	{
+	public void redValidMove2() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		final MoveResult mr = game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(-1, 0));
 		assertEquals(MoveResult.DRAW, mr);
 		game.getPrintableBoard();
 	}
-	
+
 	@Test
-	public void redValidMove3() throws HantoException
-	{
+	public void redValidMove3() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		final MoveResult mr = game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, -1));
-		
+
 		assertEquals(MoveResult.DRAW, mr);
 	}
-	
+
 	@Test
-	public void redValidMove4() throws HantoException
-	{
+	public void redValidMove4() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		final MoveResult mr = game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(-1, 1));
 		assertEquals(MoveResult.DRAW, mr);
@@ -156,18 +150,16 @@ public class AlphaHantoMasterTest
 		assertEquals(BUTTERFLY, p.getType());
 		assertEquals(HantoPlayerColor.RED, p.getColor());
 	}
-	
+
 	@Test
-	public void redValidMove5() throws HantoException
-	{
+	public void redValidMove5() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		final MoveResult mr = game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(1, 0));
 		assertEquals(MoveResult.DRAW, mr);
 	}
-	
-	@Test(expected=HantoException.class)
-	public void redTriesToPlaceButterflyAt0_0() throws HantoException
-	{
+
+	@Test(expected = HantoException.class)
+	public void redTriesToPlaceButterflyAt0_0() throws HantoException {
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 		game.makeMove(BUTTERFLY, null, new TestHantoCoordinate(0, 0));
 	}
