9254d044cd2dc3821e03f0df0ab7ad75862e14ea
Anthony J. Ruffa <ajosephruffa@gmail.com>
2014-10-08 20:28:22 -0400

    added files for epsilon hanto

---
+++ src/hanto/studentAJRZL/epsilon/EpsilonHantoGame.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package hanto.studentAJRZL.epsilon;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGame;
+import hanto.common.HantoPiece;
+import hanto.common.HantoPieceType;
+import hanto.common.MoveResult;
+
+/**
+ * Class for the epsilon hanto game.
+ * 
+ * @author anthonyjruffa
+ * 
+ */
+public class EpsilonHantoGame implements HantoGame {
+
+	@Override
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from, HantoCoordinate to)
+			throws HantoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HantoPiece getPieceAt(HantoCoordinate where) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPrintableBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
