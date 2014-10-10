539c9c8b4f557b06b802bdd734ad6069aed607f6
Anthony J. Ruffa <ajosephruffa@gmail.com>
2014-10-10 16:45:36 -0400

    added a failing epsilon test

--- test/hanto/studentAJRZL/epsilon/EpsilonHantoGameTest.java
+++ test/hanto/studentAJRZL/epsilon/EpsilonHantoGameTest.java
@@ -10,6 +10,20 @@
 
 package hanto.studentAJRZL.epsilon;
 
+import static org.junit.Assert.assertEquals;
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGameID;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.HantoTestGame;
+import hanto.common.HantoTestGameFactory;
+import hanto.common.MoveResult;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 /**
  * Tests for the epsilon hanto game.
  * 
@@ -18,4 +32,85 @@ package hanto.studentAJRZL.epsilon;
  */
 public class EpsilonHantoGameTest {
 
+	/**
+	 * Internal class for these test cases.
+	 * 
+	 * @version Sep 13, 2014
+	 */
+	class TestHantoCoordinate implements HantoCoordinate {
+		private final int x, y;
+
+		/**
+		 * Constructor for test coordinate
+		 * 
+		 * @param x
+		 * @param y
+		 */
+		TestHantoCoordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		/*
+		 * 
+		 * @see hanto.common.HantoCoordinate#getX()
+		 */
+		@Override
+		public int getX() {
+			return x;
+		}
+
+		/*
+		 * 
+		 * @see hanto.common.HantoCoordinate#getY()
+		 */
+		@Override
+		public int getY() {
+			return y;
+		}
+	}
+
+	private static HantoTestGameFactory factory;
+	private HantoTestGame game;
+
+	@BeforeClass
+	public static void init() {
+		factory = HantoTestGameFactory.getInstance();
+	}
+
+	/**
+	 * Initialize the game instance before each test.
+	 */
+	@Before
+	public void setup() {
+		game = factory.makeHantoTestGame(HantoGameID.EPSILON_HANTO);
+	}
+
+	/**
+	 * Test for a valid horse jump
+	 * 
+	 * @throws HantoException
+	 */
+	@Test
+	public void testValidHorseJumpAttempts() throws HantoException {
+		HantoTestGame.PieceLocationPair[] initialPieces = new HantoTestGame.PieceLocationPair[5];
+
+		initialPieces[0] = new HantoTestGame.PieceLocationPair(HantoPlayerColor.BLUE,
+				HantoPieceType.BUTTERFLY, new TestHantoCoordinate(0, 0));
+		initialPieces[1] = new HantoTestGame.PieceLocationPair(HantoPlayerColor.RED,
+				HantoPieceType.CRAB, new TestHantoCoordinate(0, 1));
+		initialPieces[2] = new HantoTestGame.PieceLocationPair(HantoPlayerColor.BLUE,
+				HantoPieceType.HORSE, new TestHantoCoordinate(0, -1));
+		initialPieces[3] = new HantoTestGame.PieceLocationPair(HantoPlayerColor.RED,
+				HantoPieceType.HORSE, new TestHantoCoordinate(0, 2));
+		initialPieces[4] = new HantoTestGame.PieceLocationPair(HantoPlayerColor.BLUE,
+				HantoPieceType.HORSE, new TestHantoCoordinate(0, -2));
+
+		game.initializeBoard(initialPieces);
+		game.setTurnNumber(3);
+		game.setPlayerMoving(HantoPlayerColor.RED);
+
+		assertEquals(MoveResult.OK, game.makeMove(HantoPieceType.HORSE, new TestHantoCoordinate(0,
+				2), new TestHantoCoordinate(0, -3)));
+	}
 }
