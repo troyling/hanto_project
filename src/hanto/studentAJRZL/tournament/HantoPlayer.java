cba2a1f3d1bfd51c1e418d844272226d6c07fd9a
Troy Ling <troyling14@gmail.com>
2014-10-14 16:53:10 -0400

    init tournament commit

--- src/hanto/studentAJRZL/tournament/HantoPlayer.java
+++ src/hanto/studentAJRZL/tournament/HantoPlayer.java
@@ -10,19 +10,62 @@
 
 package hanto.studentAJRZL.tournament;
 
+
+/**
+ * This is the implementation of our AI for the Hanto project
+ * 
+ * @author troyling
+ */
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGame;
 import hanto.common.HantoGameID;
+import hanto.common.HantoPieceType;
 import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+import hanto.studentAJRZL.HantoGameFactory;
 import hanto.tournament.HantoGamePlayer;
 import hanto.tournament.HantoMoveRecord;
 
+import java.util.Collection;
+
 public class HantoPlayer implements HantoGamePlayer {
+	HantoPlayerColor myColor;
+	HantoGame game;
 
 	@Override
 	public void startGame(HantoGameID version, HantoPlayerColor myColor, boolean doIMoveFirst) {
+		this.myColor = myColor;
+		if (doIMoveFirst) {
+			game = HantoGameFactory.getInstance().makeHantoGame(version, myColor);
+		} else {
+			if (myColor == HantoPlayerColor.BLUE) {
+				game = HantoGameFactory.getInstance().makeHantoGame(version, HantoPlayerColor.RED);
+			} else {
+				game = HantoGameFactory.getInstance().makeHantoGame(version, HantoPlayerColor.BLUE);
+			}
+		}
 	}
 
 	@Override
 	public HantoMoveRecord makeMove(HantoMoveRecord opponentsMove) {
+		MoveResult result = MoveResult.OK;
+		Collection<HantoCoordinate> availableCoords;
+		HantoPieceType oppoPieceType = opponentsMove.getPiece();
+		HantoCoordinate oppoFrom = opponentsMove.getFrom();
+		HantoCoordinate oppoTo = opponentsMove.getTo();
+
+		try {
+			result = game.makeMove(oppoPieceType, oppoFrom, oppoTo);
+		} catch (HantoException e) {
+			e.printStackTrace();
+		}
+		
+		if (result == MoveResult.OK) {
+			// figure out strategy
+			
+		}
+		
 		return null;
 	}
 }
